a8a55b49b98af2ad497d0d2566df01418f98c439
DurgaBhosale <durga.sonawale91@gmail.com>
2024-09-09 13:29:08 +0530

    sign up api

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,39 @@
 const express=require("express");
 const mysql=require('mysql')
-const corn=require('cors')
+const cors=require('cors')
 
 const app=express();
 app.use(cors());
+app.use(express.json());
 
 const db=mysql.createConnection({
     host:"localhost",
     user: "root",
-    password: "",
+    password: "Admin",
     database: "MCA_DEPT"
 
 })
+app.post('/signup'),(req,res)=>{
+    const sql="insert into stud_reg(fname,lname,eid,phno,gender,bdate)values(?)";
+    const values=[
+        req.body.fname,
+        req.body.lname,
+        req.body.eid,
+        req.body.phno,
+        req.body.gender,
+        req.body.bdate
+    ]
+    
+    db.query(sql,[values],(err,data)=> {
+        if(err){
+            return res.json("Error");
 
-app
+        }
+        return res.json("data");
+    })
+}
+
+
+app.listen(8081,()=>{
+    console.log("listen")
+})
